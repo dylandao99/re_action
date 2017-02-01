4343e0aa330d1da5ba5d8ab3e6c901131f6d6aee
Dylan Dao <dylandao99@gmail.com>
2017-02-01 13:22:47 -0500

    libgdx framework

---
+++ desktop/src/com/mygdx/re_action/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mygdx.re_action.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.re_action.re_action;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new re_action(), config);
+	}
+}
