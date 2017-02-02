c54b48b6adf74d04d53381209844b7b4cb1e8925
Dylan Dao <dylandao99@gmail.com>
2017-02-01 19:55:53 -0500

    Create player camera

--- core/src/com/mygdx/re_action/re_action.java
+++ core/src/com/mygdx/re_action/re_action.java
@@ -3,31 +3,33 @@ package com.mygdx.re_action;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.PerspectiveCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class re_action extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+
+	//player entities
+	private PerspectiveCamera playerCam;
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		//initialize player entities
+		playerCam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+		playerCam.update();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		playerCam.update();
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
-		img.dispose();
 	}
 }
