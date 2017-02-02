4e0ec129d592e61a6e153b9f9ad03c779c83afc4
Dylan Dao <dylandao99@gmail.com>
2017-02-01 21:39:39 -0500

    basic envrionment, modified FirstPersonCameraController for pitch, roll, and yaw

---
+++ core/src/com/mygdx/re_action/FPSController.java
@@ -0,0 +1,116 @@
+/*******************************************************************************
+ * Copyright 2011 See AUTHORS file.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+
+package com.mygdx.re_action;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Keys;
+import com.badlogic.gdx.InputAdapter;
+import com.badlogic.gdx.graphics.Camera;
+import com.badlogic.gdx.math.Quaternion;
+import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.utils.IntIntMap;
+
+/** Takes a {@link Camera} instance and controls it via w,a,s,d and mouse panning.
+ * @author badlogic */
+public class FPSController extends InputAdapter {
+    private final Camera camera;
+    private final IntIntMap keys = new IntIntMap();
+    private int STRAFE_LEFT = Keys.A;
+    private int STRAFE_RIGHT = Keys.D;
+    private int FORWARD = Keys.W;
+    private int BACKWARD = Keys.S;
+    private int ROLL_LEFT = Keys.Q;
+    private int ROLL_RIGHT = Keys.E;
+    private float velocity = 5;
+    private float degreesPerPixel = 0.5f;
+    private float rollSpeed = 0.8f;
+    private final Vector3 tmp = new Vector3();
+
+    public FPSController (Camera camera) {
+        this.camera = camera;
+    }
+
+    @Override
+    public boolean keyDown (int keycode) {
+        keys.put(keycode, keycode);
+        return true;
+    }
+
+    @Override
+    public boolean keyUp (int keycode) {
+        keys.remove(keycode, 0);
+        return true;
+    }
+
+    /** Sets the velocity in units per second for moving forward, backward and strafing left/right.
+     * @param velocity the velocity in units per second */
+    public void setVelocity (float velocity) {
+        this.velocity = velocity;
+    }
+
+    /** Sets how many degrees to rotate per pixel the mouse moved.
+     * @param degreesPerPixel */
+    public void setDegreesPerPixel (float degreesPerPixel) {
+        this.degreesPerPixel = degreesPerPixel;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        float deltaX = -Gdx.input.getDeltaX() * degreesPerPixel;
+        float deltaY = -Gdx.input.getDeltaY() * degreesPerPixel;
+        //set quaternion
+        Quaternion xRotate = new Quaternion().set(camera.up, deltaX);
+        camera.rotate(xRotate);
+        tmp.set(camera.direction).crs(camera.up).nor();
+        Quaternion yRotate = new Quaternion().set(tmp, deltaY);
+        camera.rotate(yRotate);
+        return super.mouseMoved(screenX, screenY);
+    }
+
+    public void update () {
+        update(Gdx.graphics.getDeltaTime());
+    }
+
+    public void update (float deltaTime) {
+        if (keys.containsKey(FORWARD)) {
+            tmp.set(camera.direction).nor().scl(deltaTime * velocity);
+            camera.position.add(tmp);
+        }
+        if (keys.containsKey(BACKWARD)) {
+            tmp.set(camera.direction).nor().scl(-deltaTime * velocity);
+            camera.position.add(tmp);
+        }
+        if (keys.containsKey(STRAFE_LEFT)) {
+            tmp.set(camera.direction).crs(camera.up).nor().scl(-deltaTime * velocity);
+            camera.position.add(tmp);
+        }
+        if (keys.containsKey(STRAFE_RIGHT)) {
+            tmp.set(camera.direction).crs(camera.up).nor().scl(deltaTime * velocity);
+            camera.position.add(tmp);
+        }
+        if (keys.containsKey(ROLL_LEFT)) {
+            Quaternion roll = new Quaternion().set(camera.direction, -rollSpeed);
+            camera.rotate(roll);
+        }
+        if (keys.containsKey(ROLL_RIGHT)) {
+            Quaternion roll = new Quaternion().set(camera.direction, rollSpeed);
+            camera.rotate(roll);
+        }
+        camera.update(true);
+    }
+}
--- core/src/com/mygdx/re_action/re_action.java
+++ core/src/com/mygdx/re_action/re_action.java
@@ -2,31 +2,85 @@ package com.mygdx.re_action;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.PerspectiveCamera;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.*;
+import com.badlogic.gdx.graphics.g3d.*;
+import com.badlogic.gdx.graphics.g3d.attributes.ColorAttribute;
+import com.badlogic.gdx.graphics.g3d.environment.DirectionalLight;
+import com.badlogic.gdx.graphics.g3d.utils.ModelBuilder;
+import javafx.scene.AmbientLight;
 
 public class re_action extends ApplicationAdapter {
 
 	//player entities
 	private PerspectiveCamera playerCam;
+	private FPSController fpsPlayerCam;
 
+	//world entities
+	Environment env;
+
+	//objects
+	private ModelBatch mb;
+	private Model mBgCube;
+	private ModelInstance iBgCube;
+
+	//lighting
+	private DirectionalLight dl;
 	
 	@Override
 	public void create () {
+		//delete mouse cursor
+		Gdx.input.setCursorCatched(true);
+
 		//initialize player entities
 		playerCam = new PerspectiveCamera(67, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		playerCam.far = 1000;
 
+		fpsPlayerCam = new FPSController(playerCam);
+		fpsPlayerCam.setVelocity(100f);
+
+		//TODO remove
+		playerCam.position.set(10f, 10f, 10f);
+		playerCam.lookAt(0f, 0f, 0f);
+
+		//initialize lighting
+		dl = new DirectionalLight();
+		dl.set(0.8f, 0.8f, 0.8f, -1f, -0.8f, -0.2f);
+
+		//initialize world entities
+		env = new Environment();
+		env.set(new ColorAttribute(ColorAttribute.AmbientLight, 0.4f, 0.4f, 0.4f, 1f));
+		env.add(dl);
+
+		mb = new ModelBatch();
+
+		ModelBuilder mblr = new ModelBuilder();
+
+		mBgCube = mblr.createBox(5, 5, 5,
+				new Material(ColorAttribute.createDiffuse(Color.GREEN)),
+				VertexAttributes.Usage.Position | VertexAttributes.Usage.Normal);
+
+		iBgCube = new ModelInstance(mBgCube);
+
+		//set player input control
+		Gdx.input.setInputProcessor(fpsPlayerCam);
+
+		//first camera update
 		playerCam.update();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glViewport(0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT);
 
+		mb.begin(playerCam);
+		mb.render(iBgCube, env);
+		mb.end();
+
+		fpsPlayerCam.update();
 		playerCam.update();
+
+		//TODO set player spirte to follow camera
 	}
 	
 	@Override
